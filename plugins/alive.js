8e3f99e75d73d0d51613b6b6e5ac8ab2b8a6989d
SPARK-SHADOW <ashink2019@gmail.com>
2022-10-11 22:15:04 +0530

    Poll Added & Fixed

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,6 +1,5 @@
 // by @shadow
-
-const { bot, alive_message } = require('../lib/')
+const { bot, alive_message, prefix } = require('../lib/')
 const { MODE } = require('../config');
 let MOD = MODE == 'public' ? false : true
 
@@ -16,3 +15,19 @@ bot(
      await alive_message(match, message)
    }
  )
+bot(
+	{
+		pattern: 'poll ?(.*)',
+		fromMe: MOD,
+		desc: 'Creat poll',
+		type: 'misc'
+	},
+	async (message, match) => {
+	  
+	  if (!match || match.split(":") < 2)
+      return await message.reply(
+        `_${prefix}poll question,option1,option2..._`
+      );
+    await message.poll(match)
+	}
+)
